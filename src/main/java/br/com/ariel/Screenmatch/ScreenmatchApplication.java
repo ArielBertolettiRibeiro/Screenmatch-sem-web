304e91881758d719b0c147b5288493bf62b7a2a4
Ariel Bertoletti Ribeiro <arielbertolettiribeiro@gmail.com>
2024-11-21 07:24:31 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/ariel/Screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.ariel.Screenmatch;
+
+import br.com.ariel.Screenmatch.model.DadosSerie;
+import br.com.ariel.Screenmatch.service.ConsumoAPI;
+import br.com.ariel.Screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9d55d6a7");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
